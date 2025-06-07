48919f774cf9a747ea2f66031df784d4101841cc
SZEDO-Scripts <szedoscripts01@gmail.com>
2025-06-07 14:35:32 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'Simple Car Storage Marker - Only for Whitelisted Vehicles'
+author 'SZEDO'
+
+client_script 'config.lua'
+client_script 'client.lua'
